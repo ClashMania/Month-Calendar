1183be1aaf5896835d80eb6686fb40e7c95e29e3
ClashMania <113007885+ClashMania@users.noreply.github.com>
2022-10-23 21:38:58 +0200

    Add files via upload

---
+++ MonthCalendar/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MonthCalendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mCalendar1
+            // 
+            this.mCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.mCalendar1.Name = "mCalendar1";
+            this.mCalendar1.TabIndex = 0;
+            this.mCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(71, 192);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(124, 23);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "Odaberite termin";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 242);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.mCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar mCalendar1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ MonthCalendar/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = mCalendar1.SelectionEnd - mCalendar1.SelectionStart;
+            if (ts.Days < 1)
+
+            {
+
+                MessageBox.Show("Korištenjem tipke SHIFT odaberite opseg datuma.");
+
+                return;
+
+            }
+            MessageBox.Show("Rezervirali ste usluge u trajanju od "+ts.Days.ToString() + " dana.", "Rezervacija");
+            MessageBox.Show("Vasa rezervacija počinje " +
+            mCalendar1.SelectionStart.ToShortDateString() + " a završava se" + mCalendar1.SelectionEnd.ToShortDateString());
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+    }
+}
